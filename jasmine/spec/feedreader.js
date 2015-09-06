3665466d28d954c5d30be680e1de686f24a4cddf
Matt Benjamin <matt.g.benjamin@gmail.com>
2015-09-06 19:46:28 +0900

    test: Add tests for all current features and a future 'addFeed' feature

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,52 +21,111 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
+        var feedLength = allFeeds.length;
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has a URL', function() {
+            for (var i = 0; i < feedLength; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name', function() {
+            for (var i = 0; i < feedLength; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Menu display */
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        // ensures the menu element is hidden by default
+        var $body = $('body')[0];
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        it('is hidden by default', function() {
+            expect($body.classList[0]).toBe('menu-hidden');
+        });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        //ensures the menu toggles visibility when the menu icon is clicked
+        it('toggles display', function() {
+            var $icon = $('.menu-icon-link')[0];
+            $icon.click();
+            expect($body.classList[0]).not.toBeDefined();
+            $icon.click();
+            expect($body.classList[0]).toBe('menu-hidden');
+        });
+    });
 
+    /* Initial Entries */
+    describe('Initial entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
+         * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+           $('.feed').empty();
+           loadFeed(0);
+           done();
+        });
+        it('has an entry', function() {
+            var $entries = $('.feed a .entry');
+            expect($entries).toBeDefined();
+        });
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        /* ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
+        var $oldContent;
+        beforeEach(function(done) {
+           loadFeed(0, function() {
+               $oldContent = $('.feed').html();
+               done();
+           });
+        });
+        it('changes content', function(done) {
+            loadFeed(1, function() {
+                //console.log('***OLD CONTENT*** ', $oldContent);
+                //console.log('***NEW CONTENT***', $('.feed').html());
+                expect($('.feed').html()).not.toBe($oldContent);
+                done();
+            });
+        });
+        //reset to initial state
+        afterAll(function(done) {
+            loadFeed(0, done);
+        })
+    });
+
+    /* Features not yet implemented */
+    describe('Add New Feed', function() {
+        var oldFeedCount = allFeeds.length;
+        var name = 'test';
+        var url = 'test';
+        it('should add a new RSS feed', function () {
+            addFeed(name, url);
+            expect(oldFeedCount).toBe(allFeeds.length - 1);
+        })
+    });
 }());
